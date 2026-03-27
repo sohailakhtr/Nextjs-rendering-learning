481b8777b9647f6d923e62aedd83f19289b9b617
sohailakhtr <soh7132s@gmail.com>
2026-03-27 05:34:04 +0500

    Fetching Employees

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
 import axios from "axios";
 import Link from "next/link";
 export default async function Home() {
+  const employees = await axios("http://localhost:3004/employees");
+  // console.log(Employee.data);
+  const showEmployees = employees.data.map((employee) => (
+    <div key={employee.id} className="col">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">{employee.fullname}</h3>
+          <p className="card-text">{employee.position}</p>
+          <p className="card-text">{employee.age}</p>
+          <Link href={`/employees/${employee.id}`} className="btn btn-primary">
+            Go to employee
+          </Link>
+        </div>
+      </div>
+    </div>
+  ));
   return (
     <>
-      {/* <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"> */}
-      {/* {showEmployees} */}
-      {/* </div> */}
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {showEmployees}
+      </div>
     </>
   );
 }
