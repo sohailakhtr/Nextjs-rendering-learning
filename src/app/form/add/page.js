c157901b175d6d62b889e45c218310e024b9ec9a
sohailakhtr <soh7132s@gmail.com>
2026-03-30 17:32:47 +0500

    useActionState hook

--- src/app/form/add/page.js
+++ src/app/form/add/page.js
@@ -1,7 +1,48 @@
+"use client";
+import { useActionState } from "react";
+import { addEmployee } from "@/helpers/actions";
+
 export default function AddPage() {
+  const [state, action, isPending] = useActionState(addEmployee, null);
   return (
     <>
-      <h3>ADD Employee :</h3>
+      <h3>ADD Employee</h3>
+
+      <form action={action}>
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="fullname"
+          placeholder="Enter the fullname"
+          name="fullname"
+        />
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="position"
+          placeholder="Enter the position"
+          name="position"
+        />
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="age"
+          placeholder="Enter the age"
+          name="age"
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Add empoyee
+        </button>
+      </form>
+
+      {isPending && <div>...loading</div>}
+
+      {!state?.success ? (
+        <div className="text-danger">{state?.message}</div>
+      ) : (
+        <div className="text-success">{state?.message}</div>
+      )}
     </>
   );
 }
--- src/helpers/actions.js
+++ src/helpers/actions.js
@@ -1,6 +1,28 @@
 "use server";
+
+import axios from "axios";
+import { resolve } from "styled-jsx/css";
+
 export async function counterTrigger() {
   console.log("counter Tigger");
+}
 
-  return <></>;
+export async function addEmployee(prevState, formdata) {
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  console.log(prevState);
+  try {
+    if (formdata.get("fullname") === "") {
+      throw new Error("The name is required");
+    }
+
+    await axios.post("http://localhost:3004/employees", {
+      fullname: formdata.get("fullname"),
+      position: formdata.get("position"),
+      age: formdata.get("age"),
+    });
+
+    return { success: true, message: "Name added" };
+  } catch (e) {
+    return { success: false, message: e.message };
+  }
 }
