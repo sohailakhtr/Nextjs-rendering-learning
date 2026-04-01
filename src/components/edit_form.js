4154390ed92879f1b507d9b695c20a9283fdefc8
sohailakhtr <soh7132s@gmail.com>
2026-04-01 05:19:48 +0500

    Forms with Formik and Yup

--- src/components/edit_form.js
+++ src/components/edit_form.js
@@ -1,34 +1,73 @@
-// "use client";
-// import { useFormik } from "formik";
-// import * as yup from "yup";
-// export default function EditFrom({ employee }) {
-//   return (
-//     <>
-//       <form>
-//         <input
-//           type="text"
-//           className="form-control mb-3 "
-//           id="fullname"
-//           placeholder="Enter the fullname"
-//           name="fullname"
-//         />
+"use client";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+export default function EditFrom({ employee }) {
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      id: employee.id,
+      fullname: employee.fullname,
+      position: employee.position,
+      age: employee.age,
+    },
+    validationSchema: Yup.object({
+      fullname: Yup.string().required("Sorry, fullname is required"),
+      position: Yup.string().required("Sorry, position is required"),
+      age: Yup.string().required("Sorry, age is required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
-//         <input
-//           type="text"
-//           className="form-control mb-3 "
-//           id="position"
-//           placeholder="Enter the position"
-//           name="position"
-//         />
-//         <input
-//           type="text"
-//           className="form-control mb-3 "
-//           id="age"
-//           placeholder="Enter the age"
-//           name="age"
-//         />
-//         <button className="btn btn-primary mb-3">Edit Employee</button>
-//       </form>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="fullname"
+          placeholder="Enter the fullname"
+          name="fullname"
+          {...formik.getFieldProps("fullname")}
+        />
+        {formik.errors.fullname && formik.touched.fullname ? (
+          <div className=" alert alert-warning" role="alert">
+            {formik.errors.fullname}
+          </div>
+        ) : null}
+
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="position"
+          placeholder="Enter the position"
+          name="position"
+          {...formik.getFieldProps("position")}
+        />
+        {formik.errors.position && formik.touched.position ? (
+          <div className=" alert alert-warning" role="alert">
+            {formik.errors.position}
+          </div>
+        ) : null}
+        <input
+          type="text"
+          className="form-control mb-3 "
+          id="age"
+          placeholder="Enter the age"
+          name="age"
+          {...formik.getFieldProps("age")}
+        />
+        {formik.errors.age && formik.touched.age ? (
+          <div className=" alert alert-warning" role="alert">
+            {formik.errors.age}
+          </div>
+        ) : null}
+
+        <button type="submit" className="btn btn-primary mb-3">
+          Edit Employee
+        </button>
+      </form>
+    </>
+  );
+}
