2aef965760204e86c15b2cec35c49508bdcce91c
sohailakhtr <soh7132s@gmail.com>
2026-04-03 15:47:13 +0500

    PATCH Employees

--- src/app/api/update/[id]/route.js
+++ src/app/api/update/[id]/route.js
@@ -1,3 +1,19 @@
-export async function GET() {
-  return Response.json({ text: "Hello there" });
+// export async function GET(request, { params }) {
+//   return Response.json({ param: params });
+export async function PATCH(request, { params }) {
+  const data = await request.json();
+  const ID = (await params).id;
+
+  const res = await fetch(`http://localhost:3004/employees/${ID}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    return Response.json(res.statusText, {
+      status: res.status,
+    });
+  }
+  return Response.json("ok", { status: 200 });
 }
